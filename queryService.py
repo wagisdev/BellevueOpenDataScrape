6f919a1e23f98d2cbdb1eb56a5f5043bd8b6c3d0
John S <35645358+wagisdev@users.noreply.github.com>
2023-05-20 08:52:31 -0700

    Update queryService.py
    Added resultRecordCount to halt beyond 2000 datasets. Keeps within the standard output Esri has specified by default.

--- queryService.py
+++ queryService.py
@@ -92,6 +92,7 @@ def captureDS(resultOffset):
                 'outFields': dataFields,
                 'outSR': dataWKID,
                 'resultOffset': resultOffset
+                'resultRecordCount': 2000              
                 }
 
     headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'}
